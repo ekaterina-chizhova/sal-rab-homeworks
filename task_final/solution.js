2ab1b5f239ba4487a5c51fe09bc79a792985bb2a
Ekaterina Chizhova <ekaterina.chizhova85@gmail.com>
2022-04-24 21:45:03 +0300

    task_final is done

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,18 +33,17 @@
 //    }
 // }
 function sendRequest(name, phone, address, goods, sum) {
-    //let data = {goods: [{title , count}], order: {}};
-    let data = {client: {name, phone}, order: {address, sum}, goods: [{title , count}]};
-    
+        let data = {client: {name, phone}, order: {address, sum}, goods: [ ]};
+        //let object = {title: {}, count: {}};
     let countOfGoods = goods.length;
-        for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-        data.goods.push(goods[i].count);
+        for (let i = 0; i < countOfGoods; i += 1) {
+        //data.goods.push(goods[i].object);
+        data.goods.push({title:goods[i].title, count: goods[i].count});
         }
     
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-    data.client = 'Иван';
+    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв " + address.flat;
+    data.order.sum = sum;
+    data.client = name + ' ' + phone;
     
     let jsonData = JSON.stringify({data : data});
 
